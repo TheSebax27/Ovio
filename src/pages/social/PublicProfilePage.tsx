df6ae5c016b4b676870e1486e2679860a1e3c0fe
TheSebax27 <Silvasebastian2703@gmail.com>
2026-09-03 15:45:11 -0500

    arreglo de page

--- src/pages/social/PublicProfilePage.tsx
+++ src/pages/social/PublicProfilePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import { UserPlus, UserMinus, Film, CalendarDays, MapPin, BookOpen, Wallet, Lock, Star, Music, Trophy } from 'lucide-react'
+import { UserPlus, UserMinus, Film, CalendarDays, MapPin, BookOpen, Lock, Star, Music, Trophy } from 'lucide-react'
 import { useAuth } from '../../context/AuthContext'
 import { useToast } from '../../context/ToastContext'
 import { getProfileByUsername, isFollowing, followUser, unfollowUser, getFollowers, getFollowing, getPrivacySettings } from '../../services/socialService'
